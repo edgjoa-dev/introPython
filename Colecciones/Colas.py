c83c52a18b30f43c8622c043028f1ecf1c143b58
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2023-01-13 14:48:40 -0600

    operaciones con las Colas

---
+++ Colecciones/Colas.py
@@ -0,0 +1,19 @@
+#Colas
+        #En las colas el primer elemento en entrar es el primero en salir
+        #Al agregar un elemento se coloca la final de la cola
+
+cola = ['Jose', 'Pedro', 'Maria', 'Mar']
+
+    #Agregar elemetos
+cola.append('Keyla')
+cola.append('Aylin')
+
+    #Eliminar elementos
+# cola.pop() #Elimina el último elemento
+# n = cola.pop(0) #Elimina 1er elemento
+# print(f'El elemento sustraido de la cola es: {n}')
+
+    #
+
+
+print(cola)
